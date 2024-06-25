8de5e63104e8f3e0d6b51166db23bf04f160ccbe
ablaberge <annika.laberge@icloud.com>
2024-06-25 15:30:24 -0700

    Added sampleID txt file and adjusted mapping script

--- 02_Illumina_data_processing/02_map_to_genome.sh
+++ 02_Illumina_data_processing/02_map_to_genome.sh
@@ -1,9 +1,16 @@
 ######## Mapping trim20 reads to genome ################
 
-GENOME=/ssd3/Mar_genome_analysis/genomes/Mar.3.4.6.p1_Q30Q30A.fasta
-LIST="MELC-A11_S1 PEI-DN08_S3 NYTC-C9_S2 MELC-2E11 MELC-A10 MELC-A9 PEI-DF490 DF-488 DN-HL03 FFM-19G1 FFM-20B2"
-WORK=/ssd3/Mar_genome_analysis/bwa_mapping/Mar.3.4.6.p1/all_samples
-ILLUMINA=/ssd2/Illumina_data/dedupe/trim20
+
+#ref genome
+GENOME=/fs02/Metzger/Analysis_Active/Mar_longDNA/refs/Mar.3.4.6.p1_Q30Q30A.fasta
+
+#List of sample IDs
+LIST=/Users/annikalaberge-shusterman/Desktop/PNRI/MarBTNgenome/02_Illumina_data_processing/sampleIDs.txt
+
+
+WORK=/fs02/Metzger/Analysis_Active/Mar_longDNA/workingDir
+
+ILLUMINA=/fs02/Metzger/Analysis_Active/Mar_longDNA/Illumina_data/Mar_longDNA_2024
 
 cd $WORK
 for i in $LIST
@@ -16,24 +23,7 @@ do
 	samtools index -b -@ 20 Mar.3.4.6.p1.$i.bam
 done
 
-############## Map new samples seperately
 
-LIST="DN-HL07 FFM-22A10 FFM-22F10 FFM-22A10-adductor FFM-22F10-adductor MELC-A10-siphon MELC-A11-siphon NYTC-C9-mantle PEI-DN03-siphon PEI-DN07-siphon PEI-DN08-siphon"
-# LIST1="FFM-22A10-adductor FFM-22F10-adductor MELC-A10-siphon MELC-A11-siphon NYTC-C9-mantle PEI-DN03-siphon PEI-DN07-siphon PEI-DN08-siphon"
-# LIST2="DN-HL07 FFM-22A10 FFM-22F10"
-GENOME=/ssd3/Mar_genome_analysis/genomes/Mar.3.4.6.p1_Q30Q30A.fasta
-WORK=/ssd3/Mar_genome_analysis/bwa_mapping/Mar.3.4.6.p1/all_samples
-ILLUMINA=/ssd2/Illumina_data/dedupe/2021_newsamples_trim20
 
-cd $WORK
-for i in $LIST
-do
-	bwa mem -t 80 -v 1 $GENOME $ILLUMINA/$i"_R1_001.fastq.gz" $ILLUMINA/$i"_R2_001.fastq.gz" | samtools view -b -h -@ 10 | samtools sort -O bam -@ 10 > Mar.3.4.6.p1.$i.bam
-done
 
-for i in $LIST
-do
-	samtools index -b -@ 10 Mar.3.4.6.p1.$i.bam &
-done
-wait 
 
