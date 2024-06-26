8555f33bc55501927ef1b4a7cafe8a1e361185c3
ablaberge <annika.laberge@icloud.com>
2024-06-26 13:44:56 -0700

    map script now cats from sampleIDs.txt

--- 02_Illumina_data_processing/02_map_to_genome.sh
+++ 02_Illumina_data_processing/02_map_to_genome.sh
@@ -1,18 +1,30 @@
 ######## Mapping trim20 reads to genome ################
 
+#Load dependencies 
+module load bwa/0.7.17  
+module load samtools/1.9 
+
+#set working directory
+WORK=/fs02/Metzger/Analysis_Active/Mar_longDNA/workingDir
 
 #ref genome
 GENOME=/fs02/Metzger/Analysis_Active/Mar_longDNA/refs/Mar.3.4.6.p1_Q30Q30A.fasta
+GENOMEDIR=/fs02/Metzger/Analysis_Active/Mar_longDNA/refs
 
-#List of sample IDs
-LIST=/Users/annikalaberge-shusterman/Desktop/PNRI/MarBTNgenome/02_Illumina_data_processing/sampleIDs.txt
+#Get list of sample IDs - need to 
+cd $WORK
+LIST=`cat sampleIDs.txt`
 
-
-WORK=/fs02/Metzger/Analysis_Active/Mar_longDNA/workingDir
 
 ILLUMINA=/fs02/Metzger/Analysis_Active/Mar_longDNA/Illumina_data/Mar_longDNA_2024
 
+
+#single line bwa index 
+cd $GENOMEDIR
+bwa index -a bwtsw $GENOME 
+
 cd $WORK
+
 for i in $LIST
 do
 	bwa mem -t 50 $GENOME $ILLUMINA/$i"_R1_001.fastq.gz" $ILLUMINA/$i"_R2_001.fastq.gz" | samtools view -b -h -@ 10 | samtools sort -O bam -@ 10 > Mar.3.4.6.p1.$i.bam
