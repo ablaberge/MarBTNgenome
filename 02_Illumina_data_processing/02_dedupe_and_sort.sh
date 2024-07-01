eddb9d506aeded289a0a3f82fc26f2287d8c92ca
ablaberge <annika.laberge@icloud.com>
2024-07-01 14:55:16 -0700

    Now prints stack trace for debugging and has additional flags to specify which dupes are tagged and at what pixel threshold

--- 02_Illumina_data_processing/02_dedupe_and_sort.sh
+++ 02_Illumina_data_processing/02_dedupe_and_sort.sh
@@ -1,10 +1,13 @@
 #Date : 6/27//24 
 #Author : Annika Laberge
-#Purpose : Dedupe and sort BAM outputs from mapping to reference genome. Second step in GATK pipeline's data pre-processing
+#Purpose : Mark duplicates and sort BAM outputs from mapping to reference genome. Second step in GATK pipeline's data pre-processing
 
 
 #Load dependencies 
 module load gatk/4.2.0.0 
+
+#Print stack trace
+gatk --java-options "-DGATK_STACKTRACE_ON_USER_EXCEPTION=true"
 
 #Set working directory
 WORK=/fs02/Metzger/Analysis_Active/Mar_longDNA/workingDir
@@ -16,10 +19,10 @@ LIST=`cat sampleIDs.txt`
 #Dedupe and sort using MarkDuplicatesSpark
 for i in $LIST
 do
-    gatk MarkDuplicatesSpark -I $WORK/Mar.3.4.6.p1.$i.bam \ 
-        -O $WORK/dedupe/Mar.3.4.6.p1.$i"_deduped".bam  \ 
-        -M $Work/dedupe/Mar.3.4.6.p1.$i"_Complexity_Metrics".txt \
-        --conf 'spark.executor.cores=20' \ 
+    gatk MarkDuplicatesSpark -I $WORK/Mar.3.4.6.p1.$i.bam \
+        -O $WORK/deduped/Mar.3.4.6.p1.$i_deduped.bam \
+        -M $Work/deduped/Mar.3.4.6.p1.$i_Complexity_Metrics.txt \
+        --conf 'spark.executor.cores=20' \
         --duplicate-tagging-policy All \
         --optical-duplicate-pixel-distance 50
 done
