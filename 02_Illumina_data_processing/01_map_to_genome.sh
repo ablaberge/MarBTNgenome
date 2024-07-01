bfb47b7aa2a443aec13071df83be42df92dbd448
ablaberge <annika.laberge@icloud.com>
2024-07-01 14:53:30 -0700

    Added code to add read group in header of BAM output

--- 02_Illumina_data_processing/01_map_to_genome.sh
+++ 02_Illumina_data_processing/01_map_to_genome.sh
@@ -1,8 +1,7 @@
 #Date : 6/27//24 
 #Author : Annika Laberge
 #Purpose : Map raw sequencing data to the reference genome. First data pre-processing step of GATK's germline SNP/Indel discovery pipeline.
-
-#bla bla blah
+#Note: BAM output from this ~must~ have read group in the header for MarkDuplicatesSpark (next step) to work. 
 
 #Load dependencies 
 module load bwa/0.7.17  
@@ -27,13 +26,23 @@ ILLUMINA=/fs02/Metzger/Analysis_Active/Mar_longDNA/Illumina_data/Mar_longDNA_202
 cd $GENOMEDIR
 bwa index -a bwtsw $GENOME 
 
+#Get RG ID's to add into header during bwa mem call
+for i in $LIST
+do
+	header=$(zcat $ILLUMINA/$i"_R1_001.fastq.gz" | head -n 1)
+	id=$(echo $header | head -n 1 | cut -f 1-4 -d":" | sed 's/@//' | sed 's/:/_/g')
+	echo "Read Group @RG\tID:$id\tSM:$id"_"$i\tLB:$id"_"$i\tPL:ILLUMINA"
+done
+
 #Navigate to the correct directory
-cd $WORK
+cd $WORK/RG_Added
 
 #Loop through data and map/sort
 for i in $LIST
 do
-	bwa mem -t 50 $GENOME $ILLUMINA/$i"_R1_001.fastq.gz" $ILLUMINA/$i"_R2_001.fastq.gz" | samtools view -b -h -@ 10 | samtools sort -O bam -@ 10 > Mar.3.4.6.p1.$i.bam
+	header=$(zcat $ILLUMINA/$i"_R1_001.fastq.gz" | head -n 1)
+	id=$(echo $header | head -n 1 | cut -f 3-4 -d":" | sed 's/@//' | sed 's/:/_/g')
+	bwa mem -t 50 -R "@RG\tID:$id\tSM:$i\tLB:$i"_"1\tPL:ILLUMINA" $GENOME $ILLUMINA/$i"_R1_001.fastq.gz" $ILLUMINA/$i"_R2_001.fastq.gz" | samtools view -b -h -@ 10 | samtools sort -O bam -@ 10 > Mar.3.4.6.p1.$i.bam
 done
 
 #Index data 
